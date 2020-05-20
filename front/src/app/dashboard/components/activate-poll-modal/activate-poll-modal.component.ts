b35761bd63c5b67e46702211c72a7b9fc36cd701
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-05-20 18:38:05 -0300

    MPP-11- added modal placeholder when activate button is clicked

---
+++ front/src/app/dashboard/components/activate-poll-modal/activate-poll-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activate-poll-modal',
+  templateUrl: './activate-poll-modal.component.html',
+  styleUrls: ['./activate-poll-modal.component.css']
+})
+export class ActivatePollModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- front/src/app/layouts/admin-layout/admin-layout.module.ts
+++ front/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { ActivatePollModalComponent } from '../../dashboard/components/activate-poll-modal/activate-poll-modal.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
 import { TypographyComponent } from '../../typography/typography.component';
@@ -38,6 +39,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
   ],
   declarations: [
     DashboardComponent,
+    ActivatePollModalComponent,
     UserProfileComponent,
     TableListComponent,
     TypographyComponent,
