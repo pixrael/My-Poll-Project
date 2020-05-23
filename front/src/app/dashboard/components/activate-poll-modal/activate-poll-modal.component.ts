ab88c4198a653fe19561edf5a046fbeb415a2f2c
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-05-22 21:28:50 -0300

    added time hours and minutes

--- front/src/app/dashboard/components/activate-poll-modal/activate-poll-modal.component.ts
+++ front/src/app/dashboard/components/activate-poll-modal/activate-poll-modal.component.ts
@@ -7,9 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivatePollModalComponent implements OnInit {
 
+  startDate: Date = null;
+  endDate: Date = null;
+
+  errorMsgs = {
+    start: 'Please select a date after today.\n',
+    end: 'Please select a date after the start date.\n'
+  };
+
+
+  validationData = {
+    start: {
+      isDirty: false,
+      isValid: true,
+      titleMsg: ''
+    },
+    end: {
+      isDirty: false,
+      isValid: true,
+      titleMsg: ''
+    }
+  };
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+
+  onDateChange(dateType: string, value: string) {
+    this.validationData[dateType].isDirty = true;
+    const isValid = this.checkIfFieldIsValid(dateType, value);
+    this.validationData[dateType].isValid = isValid;
+
+    if (!isValid) {
+      this.validationData[dateType].titleMsg = this.errorMsgs[dateType];
+    } else {
+      this.validationData[dateType].titleMsg = '';
+    }
+
+  }
+
+
+
+  private checkIfFieldIsValid(dateType: string, value: string): boolean {
+    const dateValue = new Date(value);
+
+    if (dateType === 'start') {
+      this.startDate = dateValue;
+      const currentDate = new Date();
+      return this.checkIfDateIsValid(currentDate, dateValue);
+    } else if (dateType === 'end') {
+      this.endDate = dateValue;
+      if (this.startDate) {
+        return this.checkIfDateIsValid(this.startDate, dateValue);
+      } else {
+        return true;
+      }
+    }
+  }
+
+  private checkIfDateIsValid(prevDate: Date, nextDate: Date): boolean {
+    return prevDate < nextDate;
   }
 
 }
